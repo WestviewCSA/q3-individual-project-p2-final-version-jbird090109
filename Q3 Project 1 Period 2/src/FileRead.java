a5b915e63bdc67472c706b83f0742dc058a30126
1915142 <1915142@01-18101.pusd.dom>
2026-02-24 11:49:50 -0800

    Used the command line thing

--- Q3 Project 1 Period 2/src/FileRead.java
+++ Q3 Project 1 Period 2/src/FileRead.java
@@ -7,10 +7,16 @@ public class FileRead {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File map = new File("EasyMap");
-		String[][][] coordinates = file(map);
-		
-		
+		if (args.length > 0) {
+			File map = new File(args[0]); 
+			String[][][] coordinates = file(map);
+			
+			if (coordinates != null) {
+				coordinates(coordinates);
+			}
+		} else {
+			System.out.println("No file name");
+		}
 	}
 	public static String[][][] file(File map){
 		try {
@@ -18,10 +24,10 @@ public class FileRead {
 			int w = scan.nextInt();
 			int h = scan.nextInt();
 			int l = scan.nextInt();
-			String[][][] coordinates = new String[w][h][l];
+			String[][][] coordinates = new String[h][w][l];
 			for(int i = 0; i < l; i++) {
 				for(int j = 0; j < h; j++) {
-					String a = " ";
+					String a = "";
 					for(int k = 0; k < w; k++) {
 						coordinates[j][k][i] = scan.next();
 						a += coordinates[j][k][i] + " ";
@@ -30,6 +36,7 @@ public class FileRead {
 					System.out.println(a);
 				}
 			}
+			scan.close();
 			return coordinates;
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -41,17 +48,20 @@ public class FileRead {
 				
 	}
 	
-	public static ArrayList<String> coordinates(File map) {
-		String[][][] a = file(map);
+	
+	
+	public static ArrayList<String> coordinates(String[][][] a) {
+		System.out.println();
 		ArrayList<String> b = new ArrayList<String>();
 		
 		for(int i = 0; i < a[0][0].length; i++) {
 			for(int j = 0; j < a.length; j++) {
 				for(int k = 0; k < a[0].length; k++) {
-					String c = a[j][k][i]  + j + k +  i;
+					String c = a[j][k][i] + " " + j + " " + k + " " +  i;
 					b.add(c);
-					
+					System.out.println(c);
 				}
+				
 		
 			}
 
